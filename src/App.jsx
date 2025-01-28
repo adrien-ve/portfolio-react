0cc96a2355caa566ccc5de8136546766aa2849a8
adrien-ve <adrien.vuillaume@outlook.fr>
2025-01-28 15:54:20 +0100

    feat: Ajout d'une section principale placeholder pour insérer le contenu

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import Header from "./components/Header";
+import MainPage from "./components/MainPage";
 
 function App() {
   return (
-    <>
+    <div className="flex flex-col h-full">
       <Header />
-    </>
+      <MainPage />
+    </div>
   );
 }
 
